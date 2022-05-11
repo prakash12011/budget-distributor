06a57dce4273172a0b60cb371c7d5a5b1b3a6223
Prakash <prakashverma12011@gmail.com>
2022-05-11 07:19:02 +0530

    Init Commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import { ref } from 'vue'
+
+const amount = ref(0);
+
+console.log(localStorage);
+</script>
+
+<template>
+  <h2>Enter Amount </h2>
+  <input type="number" name="amount" id="amount" v-model="amount" />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
