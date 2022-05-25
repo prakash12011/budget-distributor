74324782e5a53c1a34c3e3ec4c9c35b155c77cac
Prakash <prakashverma12011@gmail.com>
2022-05-25 22:18:12 +0530

    Sass file included in App.js

--- src/App.vue
+++ src/App.vue
@@ -136,21 +136,6 @@ const addToTotal = (val) => {
   </div>
 </template>
 
-<style>
-*,
-*::before,
-*::after {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-}
-
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+@import 'src/assets/scss/style';
 </style>
